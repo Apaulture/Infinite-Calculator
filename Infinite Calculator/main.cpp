c60695d3ca69fbaf49ac8ab158238ad8459eff62
Apaulture <apaulture@gmail.com>
2020-02-12 10:12:31 +0700

    Initial Commit

---
+++ Infinite Calculator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Infinite Calculator
+//
+//  Created by Apaulture on 2/12/20.
+//  Copyright © 2020 Apaulture. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
